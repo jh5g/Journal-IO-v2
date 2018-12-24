190e156bac193cd457419ba48afd2079a136e84b
jh5g <31213220+jh5g@users.noreply.github.com>
2018-12-24 15:48:12 +0000

    Now includes Search Function.
    v2 is now fully functional

---
+++ Journal-IO-v2/SearchWIndow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Journal_IO_v2
+{
+    /// <summary>
+    /// Interaction logic for SearchWIndow.xaml
+    /// </summary>
+    public partial class SearchWIndow : Window
+    {
+        public List<entry> entries = new List<entry>();
+        List<entry> filteredEntries { get; set; }
+        System.Globalization.CultureInfo ukCulture = new System.Globalization.CultureInfo("en-GB");
+
+        public SearchWIndow()
+        {
+            InitializeComponent();
+        }
+
+        public string FormatForOutput() //Turns list into string for output
+        {
+            filteredEntries = filteredEntries.OrderBy(d => d.Date).ToList();
+            string output = "";
+            foreach (entry entry in filteredEntries)
+            {
+                output += entry.UnformattedDate;
+                output += " : ";
+                output += entry.Entry;
+                output += "\n";
+            }
+            return output;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            String date = DateEntry.Text;
+            String Text = TextEntry.Text;
+            filteredEntries = entries.ToList<entry>();
+            if (date != "")
+            {
+                foreach (entry ent in entries)
+                {
+                    if (!( ent.UnformattedDate.Contains(date)))
+                    {
+                        filteredEntries.Remove(ent);
+                    }
+                }
+            }
+            if (Text != "")
+            {
+                foreach (entry ent in entries)
+                {
+                    if (!( ent.Entry.Contains(Text)))
+                    {
+                        filteredEntries.Remove(ent);
+                    }
+                }
+            }
+            if (date != "" || Text != "")
+            {
+                Output.Text = FormatForOutput();
+            }
+            else
+            {
+                Window1 window1 = new Window1("No Search method was found");
+                window1.Title = "Warning";
+                window1.Cancel.Content = "Ok";
+                window1.Confirm.Visibility = Visibility.Hidden;
+                window1.Show();
+            }
+        }
+    }
+}
---
+++ Journal-IO-v2/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Journal_IO_v2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(string question)
+        {
+            InitializeComponent();
+            QuestionBox.Text = question;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
